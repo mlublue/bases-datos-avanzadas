7d4ca7cd30fe872ca2dab89a0b62e76cb9aa1c1f
CarolineG03 <caroldi95@gmail.com>
2015-11-18 21:00:42 -0600

    Se modificó teamjson.php

--- teamjson_json.php
+++ teamjson_json.php
@@ -73,7 +73,8 @@ if (file_exists($xml)) {
 <body background="http://cdn.wonderfulengineering.com/wp-content/uploads/2013/11/apple-wallpaper-background-2.jpg">
 
 	<h1>XML a JSON</h1>
-	<h2> Ya esta convertido desde un archivo XML</h2>
+	<h2>Aun no convierte desde un archivo XML</h2>
+  <p>Perdone la espera</p>
 	
 	
 </body>
